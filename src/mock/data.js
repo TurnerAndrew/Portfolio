c3918e14418adabdd1b1befecf84a9dd2520adba
Andrew Turner <turner.andrew0@gmail.com>
2021-02-11 10:41:35 -0600

    link update

--- src/mock/data.js
+++ src/mock/data.js
@@ -78,7 +78,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: 'https://www.linkedin.com/in/andrewturner0/',
+      url: 'https://www.linkedin.com/in/andrewturner0',
     },
     {
       id: nanoid(),
@@ -90,5 +90,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
